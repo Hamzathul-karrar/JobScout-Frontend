74ce6a9f3ff6e95c9a82e422222253a4fb38d4e4
Hamzathul-Karrar <hamhamza9749@gmail.com>
2025-10-02 13:01:56 +0530

    Updated serpapi tool tip

--- src/components/Register/RegistrationForm.jsx
+++ src/components/Register/RegistrationForm.jsx
@@ -78,7 +78,7 @@ const RegistrationForm = ({
           onBlur={(value) => handleFieldBlur("serpApiKey", value)}
           error={errors.serpApiKey}
           required
-          tooltipText="SerpApi Key should be Valid."
+          tooltipText="SerpApi Key should be Valid. Get it from https://serpapi.com/users/sign_up?plan=free"
           className="full-row"
         />
       </div>
